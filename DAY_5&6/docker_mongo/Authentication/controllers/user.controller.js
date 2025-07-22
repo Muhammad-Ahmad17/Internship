0c6384980fd520f258d98cc387f52365d0e5545b
Muhammad Ahamd <muhammadahmad171105@gmail.com>
2025-07-22 18:36:00 +0500

    feat: Connected local MongoDB to Node.js app, then containerized the complete app using Docker (Internship Day 6)

---
+++ DAY_5&6/docker_mongo/Authentication/controllers/user.controller.js
@@ -0,0 +1,55 @@
+const User = require('../models/user.model');
+const bcrypt = require('bcryptjs'); 
+const jwt = require('jsonwebtoken');
+
+exports.registerUser = async (req,res) => {
+    try {
+        const { name, email, password } = req.body;
+        // Check if user already exists
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+        // Hash the password 
+        const hashedPassword = await bcrypt.hash(password, 10);
+        // Create a new user
+        const user = new User({ name, email, password: hashedPassword });
+        await user.save();
+        res.status(201).json({ message: 'User registered successfully' } , { name: user.name } ,{email: user.email} , {password:user.password} );
+
+    } catch (error) {
+        console.error('Error registering user:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
+exports.loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        // Find user by email
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid email or password' });
+        }
+        // Check password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid email or password' });
+        }
+        // Generate JWT token
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET || 'your_jwt_secret', { expiresIn: '1h' });
+        res.status(200).json({ token });
+    } catch (error) {
+        console.error('Error logging in user:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+exports.userProfile = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id);
+        res.status(200).json(user);
+    } catch (error) {
+        console.error('Error fetching user profile:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
