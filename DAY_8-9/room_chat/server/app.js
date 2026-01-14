d4ae8f309b23926688396e395e0392f0905c8e2c
Muhammad-Ahmad17 <muhammadahmad171105@gmail.com>
2026-01-15 03:46:05 +0500

    feat: implement full chat room flow with join, send, and leave events using Socket.IO (Internship Day 8&9)

--- DAY_8-9/room_chat/server/app.js
+++ DAY_8-9/room_chat/server/app.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 // Middleware for CORS
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://internship-vcg1.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true,
   })
@@ -18,7 +18,7 @@ app.use(
 // Socket.IO setup
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://internship-vcg1.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -70,6 +70,8 @@ socket.on("leave-room", () => {
 
 });
 
-server.listen(5000, () => {
-  console.log("🚀 Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
